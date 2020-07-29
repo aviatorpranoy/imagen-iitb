d86a2349fe1218e0e5b16b7740501546f4119c35
Pranoy Ray <aviatorpranoy@gmail.com>
2020-07-30 02:10:32 +0530

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,27 @@
+var btn = doc.getElementByID("btn");
+var res= doc.getElementByID("result");
+
+//Ajax call function
+btn.addEventListener("click",function() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET,'data.json')
+    ourRequest.onload = function(){
+        var ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData[0]);
+    };
+    ourRequest.send();
+});
+
+//Display Output Function (add html to the page)
+function renderHTML(data){
+    var htmlString="";
+
+    //AJAX call to load data from python script
+ 
+
+
+    //for(i=0;i<data.length;i++){
+    htmlString+= "<p>"+"The constants of elasticity of "+data[i].name+", "+data[i+1].name+" and "+data[i+2].name+" is "+"</p>";
+
+    res.insertAdjacentHTML('beforeend',htmlString);
+}
