5dd342456e88a1b3966047ccbbb53a7defd49a0a
Pranoy Ray <aviatorpranoy@gmail.com>
2020-07-30 20:32:51 +0530

    Add files via upload

--- flaskblog.py
+++ flaskblog.py
@@ -48,7 +48,7 @@ def process():
 		second = clicked.split(";")[1]
 		third = clicked.split(";")[2]
 
-		sums = int(first) + int(second) + int(third)+3
+		sums = 3 + int(first) + int(second) + int(third)
 		print(sums)
 		return json.dumps({"sum" : sums})
 
