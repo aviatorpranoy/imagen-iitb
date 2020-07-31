bf482499fc8ad7a0622f2418aa22068df13a2e87
Pranoy Ray <aviatorpranoy@gmail.com>
2020-07-31 22:28:55 +0530

    Add files via upload

--- render.py
+++ render.py
@@ -1,3 +1,15 @@
-def add3(a,b,c):
-    d=int(a) + int(b) + int(c) + 3
+from flask import Flask, render_template, url_for, request, jsonify, make_response
+
+def execute2(a,b):
+    print("execute2() has been executed")
+    print(type(a))
+    d=0
+    d= int(a) + int(2) + int(b)
     return d
+
+def execute3(d,c):
+    print("execute3() has been executed")
+    print(type(d))
+    e=0
+    e= d + int(1) + c
+    return e
