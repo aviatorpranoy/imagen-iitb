b75e41f4a8e9cb9e75c00aa2f59f12ea2056772d
Pranoy Ray <aviatorpranoy@gmail.com>
2020-08-10 02:33:33 +0530

    Add files via upload

--- imagenflask/models.py
+++ imagenflask/models.py
@@ -1,40 +1,46 @@
 from datetime import datetime
-from imagenflask import login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from imagenflask import db, login_manager, app
 from flask_login import UserMixin
-from flask_login import current_user
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-class User:
-  def __init__(self, username, email, affiliation):
-    self.username = username
-    self.email = email
-    self.affiliation = affiliation
-
-  def myfunc(abc):
-    print("Hello my username is " + abc.username)
-    print("My email is " + abc.email)
-    print("My affiliation is " + abc.affiliation)
-
-#class User(db):
-    #id = db.Column(db.Integer, primary_key=True)
-#    username = current_user.username
-#    email = current_user.email
-#    image_file = current_user.image_file
-#    password = current_user.password
-#    posts = current_user.posts
-
-    #def __repr__(self):
-    #    return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-#class Post(db):
-    #id = db.Column(db.Integer, primary_key=True)
-    #title = current_user.title
-    #date_posted = current_user.date_posted
-    #content = current_user.content
-    #user_id = current_user.user_id
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(100), unique=False, nullable=False)
+    affiliation = db.Column(db.String(100), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
-    #def __repr__(self):
-        #return f"Post('{self.title}', '{self.date_posted}')"
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.affiliation}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
