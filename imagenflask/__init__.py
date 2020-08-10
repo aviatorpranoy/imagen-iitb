c983ccd658a45331f330918103c09250230b35c4
Pranoy Ray <aviatorpranoy@gmail.com>
2020-08-10 13:47:43 +0530

    Update __init__.py

--- imagenflask/__init__.py
+++ imagenflask/__init__.py
@@ -6,7 +6,7 @@ from flask_mail import Mail
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'imageniitb_alankar2020'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
