3731f7f24e71b65a9b38fab61e7ed47cc99a49f9
Pranoy Ray <aviatorpranoy@gmail.com>
2020-08-10 15:29:05 +0530

    Add files via upload

--- imagenflask/__init__.py
+++ imagenflask/__init__.py
@@ -6,8 +6,8 @@ from flask_mail import Mail
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = 'imageniitb_alankar2020'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
