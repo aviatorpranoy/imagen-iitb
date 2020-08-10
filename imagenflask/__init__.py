3cc2c0971820d91f4f5b96e7b60d9dc3405945e6
Pranoy Ray <aviatorpranoy@gmail.com>
2020-08-10 13:35:43 +0530

    Update __init__.py

--- imagenflask/__init__.py
+++ imagenflask/__init__.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'imageniitb_alankar2020'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
